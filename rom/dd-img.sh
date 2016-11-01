11294a3bb3d5de76f6fd7748902f0852f5976c95
astoria-d@fc <astoria-d@fc>
2016-11-01 14:34:33 +0900

    make and shells clean up

--- rom/dd-img.sh
+++ rom/dd-img.sh
@@ -20,11 +20,11 @@ echo "processing...."
 dd if=$in_file of=$out_file1 bs=16 skip=1 count=2048 2> /dev/null
 dd if=$in_file of=$out_file2 bs=16 skip=2049 2> /dev/null
 
-objcopy -I binary -O ihex $in_name-prg.bin sample1-prg.hex
-objcopy -I binary -O ihex $in_name-chr.bin sample1-chr.hex
+objcopy -I binary -O ihex $in_name-prg.bin $in_name-prg.hex
+objcopy -I binary -O ihex $in_name-chr.bin $in_name-chr.hex
 
 #8k img creation
-dd if=$in_name-prg.bin of=$in_name-prg-8k.bin bs=512 count=16
-objcopy -I binary -O ihex $in_name-prg-8k.bin sample1-prg-8k.hex
+#dd if=$in_name-prg.bin of=$in_name-prg-8k.bin bs=512 count=16
+#objcopy -I binary -O ihex $in_name-prg-8k.bin sample1-prg-8k.hex
 
 echo "done."
