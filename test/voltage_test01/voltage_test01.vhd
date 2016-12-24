daf4e199894813d074938fa743ec1de13590e510
astoria-d <astoria-d@mail.goo.ne.jp>
2016-12-24 11:29:33 +0900

    volatage test project created.

---
+++ test/voltage_test01/voltage_test01.vhd
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.conv_integer;
+
+--  
+--   MOTO NES FPGA On DE0-CV Environment Virtual Cuicuit Board
+--   All of the components are assembled and instanciated on this board.
+--  
+
+entity voltage_test01 is 
+    port (
+--logic analyzer reference clock
+    signal dbg_base_clk: out std_logic;
+    
+--NES instance
+        pi_base_clk 	: in std_logic;
+        pi_sw          : out std_logic_vector(9 downto 0);
+        pi_btn         : out std_logic_vector(3 downto 0);
+        po_led_r       : out std_logic_vector(9 downto 0);
+        po_led_g       : out std_logic_vector(7 downto 0)
+         );
+end voltage_test01;
+
+architecture rtl of voltage_test01 is
+
+begin
+
+end rtl;
