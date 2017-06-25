85c60ab2d25bcfd6694f7b9d360eea0837876539
astoria-d <astoria-d@home-ubuntu>
2017-06-25 19:17:55 +0900

    i2c module update

---
+++ test/i2c_test/i2c.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.conv_integer;
+use ieee.std_logic_arith.conv_std_logic_vector;
+use ieee.std_logic_unsigned.all;
+
+entity i2c_slave is 
+    port (
+        pi_i2c_scl      : in std_logic;
+        pio_i2c_sda     : inout std_logic
+    );
+end i2c_slave;
+
+architecture rtl of i2c_slave is
+
+begin
+    pio_i2c_sda <= 'Z';
+end rtl;
--- test/i2c_test/i2c_test.vhd
+++ test/i2c_test/i2c_test.vhd
@@ -4,17 +4,16 @@ use ieee.std_logic_unsigned.conv_integer;
 use ieee.std_logic_arith.conv_std_logic_vector;
 use ieee.std_logic_unsigned.all;
 
---entity rom_test01 is 
 entity i2c_test is 
     port (
         pi_base_clk    : in std_logic;
-        pi_reset_n	  : in std_logic;
-        pi_key		  	  : in std_logic_vector(3 downto 0);
-        pi_sw		  	  : in std_logic_vector(9 downto 0);
-        po_led		  	  : out std_logic_vector(9 downto 0);
+        pi_reset_n     : in std_logic;
+        pi_key             : in std_logic_vector(3 downto 0);
+        pi_sw             : in std_logic_vector(9 downto 0);
+        po_led             : out std_logic_vector(9 downto 0);
 
-        po_i2c_scl	  : out std_logic;
-        po_i2c_sda	  : out std_logic;
+        pio_i2c_scl     : inout std_logic;
+        pio_i2c_sda     : inout std_logic;
 
 --        pi_phi2             : in std_logic;
 --        pi_prg_ce_n         : in std_logic;
@@ -28,22 +27,34 @@ entity i2c_test is
 --        po_chr_data         : out std_logic_vector(7 downto 0);
         po_dbg_cnt          : out std_logic_vector (63 downto 0)
          );
---end rom_test01;
 end i2c_test;
 
---architecture rtl of rom_test01 is
 architecture rtl of i2c_test is
+
+
+component i2c_slave port (
+    pi_i2c_scl      : in std_logic;
+    pio_i2c_sda     : inout std_logic
+    );
+end component;
+
 
 signal reg_dbg_cnt      : std_logic_vector (63 downto 0);
 
 begin
 
-	 po_led <= reg_dbg_cnt(32 downto 23);
+    po_led <= reg_dbg_cnt(32 downto 23);
     po_dbg_cnt <= reg_dbg_cnt;
-	 
-	 ---slow i2c clock for debugging...
-	 po_i2c_scl <= reg_dbg_cnt(23);
-	 
+    
+    ---slow i2c clock for debugging...
+--    po_i2c_scl <= reg_dbg_cnt(23);
+    i2c_slave_inst : i2c_slave port map (
+        pio_i2c_scl,
+        pio_i2c_sda
+    );
+
+    pio_i2c_scl <= 'Z';
+
     deb_cnt_p : process (pi_base_clk, pi_reset_n)
 use ieee.std_logic_unsigned.all;
     variable cnt : integer;
@@ -53,11 +64,11 @@ use ieee.std_logic_unsigned.all;
             cnt := 0;
         elsif (rising_edge(pi_base_clk)) then
             if (cnt = 0) then
-					if (pi_sw(0) = '1') then
-						 --debug count is half cycle because too fast to capture in st ii.
-						 reg_dbg_cnt <= reg_dbg_cnt + 1;
-						 cnt := 1;
-					end if;
+               if (pi_sw(0) = '1') then
+                   --debug count is half cycle because too fast to capture in st ii.
+                   reg_dbg_cnt <= reg_dbg_cnt + 1;
+                   cnt := 1;
+                end if;
             else
                 cnt := 0;
             end if;
