41bae2ccdfafb8d417409b93e558017570e073dd
astoria-d <astoria-d@mail.goo.ne.jp>
2017-07-16 06:07:49 +0900

    i2c status update

--- test/i2c_test/i2c.vhd
+++ test/i2c_test/i2c.vhd
@@ -16,15 +16,54 @@ end i2c_slave;
 architecture rtl of i2c_slave is
 
 
-type i2c_bus_stat is (idle, start,
+type i2c_sp_stat is (
+    start, stop
+    );
+
+type i2c_bus_stat is (
+    idle,
     a6, a5, a4, a3, a2, a1, a0, rw, a_ack,
-    d7, d6, d5, d4, d3, d2, d1, d0, d_ack,
-    stop);
+    d7, d6, d5, d4, d3, d2, d1, d0, d_ack
+    );
+
+signal reg_cur_sp       : i2c_sp_stat;
+signal reg_next_sp      : i2c_sp_stat;
+signal reg_old_sda      : std_logic;
 
 signal reg_cur_state      : i2c_bus_stat;
 signal reg_next_state     : i2c_bus_stat;
 
 begin
+
+    --start/stop status.
+    set_sp : process (pi_rst_n, pi_base_clk)
+    begin
+        if (pi_rst_n = '0') then
+            reg_cur_sp <= stop;
+            reg_old_sda <= '1';
+        elsif (rising_edge(pi_base_clk)) then
+            reg_cur_sp <= reg_next_sp;
+            reg_old_sda <= pio_i2c_sda;
+        end if;--if (pi_rst_n = '0') then
+    end process;
+
+    next_sp : process (pi_rst_n, pi_base_clk)
+    begin
+        if (pi_rst_n = '0') then
+            reg_next_sp <= stop;
+        elsif (rising_edge(pi_base_clk)) then
+            case reg_cur_sp is
+                when stop =>
+                    if (pi_i2c_scl = '1' and reg_old_sda = '1' and pio_i2c_sda = '0') then
+                        reg_next_sp <= start;
+                    end if;
+                when start =>
+                    if (pi_i2c_scl = '1' and reg_old_sda = '0' and pio_i2c_sda = '1') then
+                        reg_next_sp <= stop;
+                    end if;
+            end case;
+        end if;--if (pi_rst_n = '0') then
+    end process;
 
     --i2c bus state machine (state transition)...
     set_stat_p : process (pi_rst_n, pi_base_clk)
@@ -32,141 +71,65 @@ begin
         if (pi_rst_n = '0') then
             reg_cur_state <= idle;
         elsif (rising_edge(pi_base_clk)) then
-            reg_cur_state <= reg_next_state;
+            if (reg_cur_sp = start) then
+                reg_cur_state <= reg_next_state;
+            else
+                reg_cur_state <= idle;
+            end if;
         end if;--if (pi_rst_n = '0') then
     end process;
 
     --state change to next.
-    next_stat_p : process (reg_cur_state, pi_i2c_scl, pio_i2c_sda)
+    next_stat_p : process (pi_rst_n, pi_i2c_scl)
     begin
-        case reg_cur_state is
-            when idle =>
-                if (pi_i2c_scl = '1' and pio_i2c_sda = '0') then
-                    reg_next_state <= start;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when start =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= a6;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when a6 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= a5;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when a5 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= a4;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when a4 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= a3;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when a3 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= a2;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when a2 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= a1;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when a1 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= a0;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when a0 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= rw;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when rw =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= a_ack;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when a_ack =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= d7;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when d7 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= d6;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when d6 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= d5;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when d5 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= d4;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when d4 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= d3;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when d3 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= d2;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when d2 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= d1;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when d1 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= d0;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when d0 =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= d_ack;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when d_ack =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= stop;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-            when stop =>
-                if (pi_i2c_scl = '1') then
-                    reg_next_state <= idle;
-                else
-                    reg_next_state <= reg_cur_state;
-                end if;
-        end case;
+        if (pi_rst_n = '0') then
+            reg_next_state <= idle;
+        elsif (rising_edge(pi_i2c_scl)) then
+            if (reg_cur_sp = start) then
+                case reg_cur_state is
+                    when idle =>
+                        reg_next_state <= a6;
+                    when a6 =>
+                        reg_next_state <= a5;
+                    when a5 =>
+                        reg_next_state <= a4;
+                    when a4 =>
+                        reg_next_state <= a3;
+                    when a3 =>
+                        reg_next_state <= a2;
+                    when a2 =>
+                        reg_next_state <= a1;
+                    when a1 =>
+                        reg_next_state <= a0;
+                    when a0 =>
+                        reg_next_state <= rw;
+                    when rw =>
+                        reg_next_state <= a_ack;
+                    when a_ack =>
+                        reg_next_state <= d7;
+                    when d7 =>
+                        reg_next_state <= d6;
+                    when d6 =>
+                        reg_next_state <= d5;
+                    when d5 =>
+                        reg_next_state <= d4;
+                    when d4 =>
+                        reg_next_state <= d3;
+                    when d3 =>
+                        reg_next_state <= d2;
+                    when d2 =>
+                        reg_next_state <= d1;
+                    when d1 =>
+                        reg_next_state <= d0;
+                    when d0 =>
+                        reg_next_state <= d_ack;
+                    when d_ack =>
+                        reg_next_state <= idle;
+                end case;
+            else
+                reg_next_state <= idle;
+            end if;--if (reg_cur_sp = start) then
+        end if;--if (pi_rst_n = '0') then
     end process;
 
     pio_i2c_sda <= 'Z';
--- test/i2c_test/simulation/modelsim/i2c_test_run_msim_rtl_vhdl.do
+++ test/i2c_test/simulation/modelsim/i2c_test_run_msim_rtl_vhdl.do
@@ -10,24 +10,28 @@ vcom -93 -work work {D:/daisuke/nes/repo/super-duper-nes/test/i2c_test/i2c_test.
 
 vcom -93 -work work {D:/daisuke/nes/repo/super-duper-nes/test/i2c_test/simulation/modelsim/testbench_i2c_test.vhd}
 
-vsim -t 1ps -L altera -L lpm -L sgate -L altera_mf -L altera_lnsim -L cyclonev -L rtl_work -L work -voptargs="+acc"  testbench_i2c_test
+vsim -t 1ps -L altera -L lpm -L sgate -L altera_mf -L altera_lnsim -L cyclonev -L rtl_work -L work -voptargs="ê "  testbench_i2c_test
 
 #add wave *
 add wave -label pi_reset_n  sim:/testbench_i2c_test/sim_board/pi_reset_n
 #add wave -label pi_base_clk sim:/testbench_i2c_test/sim_board/pi_base_clk
 
-add wave -label pio_i2c_sda  sim:/testbench_i2c_test/sim_board/pio_i2c_sda
 add wave -label pi_i2c_scl  sim:/testbench_i2c_test/sim_board/pi_i2c_scl
 
-add wave -divider i2c_slave
-add wave -label reg_cur_state   sim:/testbench_i2c_test/sim_board/i2c_slave_inst/reg_cur_state
+#add wave -label reg_old_sda     sim:/testbench_i2c_test/sim_board/i2c_slave_inst/reg_old_sda
+add wave -label pio_i2c_sda  sim:/testbench_i2c_test/sim_board/pio_i2c_sda
 
+
+add wave -divider i2c_slave
+add wave -label reg_cur_sp      sim:/testbench_i2c_test/sim_board/i2c_slave_inst/reg_cur_sp
+#add wave -label reg_next_sp     sim:/testbench_i2c_test/sim_board/i2c_slave_inst/reg_next_sp
+add wave -label reg_cur_state   sim:/testbench_i2c_test/sim_board/i2c_slave_inst/reg_cur_state
 
 view structure
 view signals
 
 #run -all
 
-run 100 us
+run 200 us
 wave zoom full
 
