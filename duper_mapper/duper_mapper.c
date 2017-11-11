4cde8b9df88cf58030bf05e9f2874188506aafdd
astoria-d <astoria-d@home>
2017-11-11 19:20:31 +0900

    mapper i2c fifo emulated.

--- duper_mapper/duper_mapper.c
+++ duper_mapper/duper_mapper.c
@@ -1,5 +1,9 @@
 #include <dlfcn.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #define __mapper_impl__
 
@@ -20,14 +24,40 @@ static unsigned char fifo_read_reg;
 static unsigned char fifo_write_reg;
 static unsigned char fifo_status_reg;
 
+#define I2C_INPUT   "i2c_in"
+
 /*
 0xfff9	w: push fifo register
-	    r: pop fifo register
+	r: pop fifo register
 0xfff8	fifo status register
+	
+	
+	firo status register
+
+bit	
+0	write fifo empty
+1	write fifo full
+2	always 0
+3	always 0
+4	read fifo empty
+5	read fifo full
+6	always 0
+7	always 0
 */
 
 int mp_init(mp_set_addr_t set_a_func, mp_get_data_t get_d_func, mp_set_data_t set_d_func) {
     printf("duper mapper init...\n");
+
+    printf("duper mapper i2c emuration:\n");
+    printf("    to send a data to NES: [i2c_in].\n");
+    printf("    to receive a data from NES: [i2c_out].\n");
+    printf("    you can send i2c one character at a time.\n");
+    printf("    > echo -n \"a\" > i2c_in\n");
+    printf("    you can receive from i2c\n");
+    printf("    > cat i2c_out\n");
+    printf(" #write fifo empty/full is not implemented.\n");
+    printf(" #read fifo full register is not implemented.\n");
+
     set_rom_addr = set_a_func;
     get_rom_data = get_d_func;
     set_rom_data = set_d_func;
@@ -53,19 +83,52 @@ void mp_set_addr(unsigned short addr) {
 }
 
 unsigned char mp_get_data(void) {
+    int in_fifo;
+
     if (mapper_access_addr == 0x7FF9) {
         /*read fifo reg.*/
+
+        in_fifo = open(I2C_INPUT, O_RDONLY);
+        if (in_fifo != -1) {
+            read(in_fifo, &fifo_read_reg, 1);
+            close(in_fifo);
+            /*clean up fifo.*/
+            remove(I2C_INPUT);
+        }
+
         printf("duper mapper get fifo: %02x\n", fifo_read_reg);
         return fifo_read_reg;
     }
     else if (mapper_access_addr == 0x7FF8) {
-        /*read fifo status reg.*/
-        printf("duper mapper get status: %02x\n", fifo_status_reg);
-        return fifo_read_reg;
+
+/*
+status bit...
+4	read fifo empty
+5	read fifo full
+*/
+        in_fifo = open(I2C_INPUT, O_RDONLY);
+        if (in_fifo == -1) {
+            /*fifo empty.*/
+            fifo_status_reg |= 0x10;
+        }
+        else {
+/*
+            printf("fifo input...\n");
+*/
+            fifo_status_reg &= ~0x10;
+            close(in_fifo);
+        }
+
+        /*TODO: check write full*/
+
+        printf("duper mapper get fifo status: %02x\n", fifo_status_reg);
+        return fifo_status_reg;
     }
     else {
         /*read rom.*/
-        printf("duper mapper get data: %04x\n", mapper_access_addr);/**/
+        /*
+        printf("duper mapper get data: %04x\n", mapper_access_addr);
+        */
         return (*get_rom_data)();
     }
 }
