da7c65398781a39301670dd5d9efd62b90ad9dca
astoria-d <astoria-d@home-ubuntu>
2018-01-03 19:22:39 +0900

    now duper nes device is probedgit add -A

--- duper_linux/bbb_tty/bt_i2c.c
+++ duper_linux/bbb_tty/bt_i2c.c
@@ -2,8 +2,8 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/platform_device.h>
 #include <linux/of.h>
+#include <linux/i2c.h>
 
 #include "bbb_tty.h"
 
@@ -11,20 +11,13 @@
    driver prove/remove not working...
    must consider alternative method to access platform device...
 */
-static int bt_i2c_probe(struct platform_device *pdev) {
-    struct resource *res;
+static int bt_i2c_probe (struct i2c_client *client, const struct i2c_device_id *id) {
 
     printk(KERN_INFO "bt_i2c_probe.\n");
-    res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-    if (!res) {
-        printk(KERN_INFO "platform_get_resource failed..\n");
-    }
-    printk(KERN_INFO "res:%08x..\n", (unsigned int)res);
-
     return 0;
 }
 
-static int bt_i2c_remove(struct platform_device *pdev) {
+static int bt_i2c_remove(struct i2c_client *client) {
     printk(KERN_INFO "bt_i2c_remove.\n");
     return 0;
 }
@@ -33,54 +26,64 @@ static const struct bt_platform_data bt_pdata = {
     .data = 1,
 };
 
-static const struct of_device_id bt_match[] = {
-    {
-        /*match with dts entry.*/
-        .compatible = "fujibon,duper_nes",
-        .data = &bt_pdata,
-    },
-    { },
-};
-MODULE_DEVICE_TABLE(of, bt_match);
-
-/*
-static const struct platform_device_id bt_id_table[] = {
-    {
-        //.name   = "omap_rtc",
-        .name   = "bbb_tty",
-        .driver_data    = &bt_pdata,
-    },
+static const struct i2c_device_id bt_id_table[] = {
+    {"duper_nes", 0 },
     { }
 };
-MODULE_DEVICE_TABLE(platform, bt_id_table);
+MODULE_DEVICE_TABLE(i2c, bt_id_table);
+/*
 */
-
-
-static struct platform_driver bt_i2c_driver = {
-    .probe      = bt_i2c_probe,
-    .remove     = bt_i2c_remove,
-    .driver     = {
-        /*this name must be unique..*/
-        .name   = "bbb_tty",
-        .of_match_table = of_match_ptr(bt_match),
-    },
-    //.id_table = bt_id_table,
-};
 
 /*
-module_platform_driver(bt_i2c_driver);
+static struct i2c_board_info duper_nes_devices[] = {
+    {
+        .type = "duper_nes",
+        .addr = 0x31,
+    },
+};
 */
+
+
+static struct i2c_driver  bt_i2c_driver = {
+    .driver     = {
+        .name   = "duper_nes",
+    },
+    .probe      = bt_i2c_probe,
+    .remove     = bt_i2c_remove,
+    .id_table = bt_id_table,
+};
+
+
+static struct i2c_client *client;
 
 int bt_i2c_init(void){
     int ret;
+    struct i2c_adapter *adapter;
 
-    ret = platform_driver_register(&bt_i2c_driver);
-    printk(KERN_INFO "bt_i2c_init ret:%d.\n", ret);
+    /*ret = i2c_register_board_info(1, duper_nes_devices, ARRAY_SIZE(duper_nes_devices));
+    printk(KERN_INFO "register i2c board ret:%d.\n", ret);*/
+
+    ret = i2c_add_driver(&bt_i2c_driver);
+    printk(KERN_INFO "add i2c driver ret:%d.\n", ret);
+
+    /*
+    struct i2c_board_info info = {
+        .type = "bbb_tty_core",
+        .addr = 0x44,
+    };
+
+    adapter = i2c_get_adapter(1);
+    if (!adapter) return -EINVAL;
+
+    client = i2c_new_device(adapter, &info);
+    if (!client) return -EINVAL;
+*/
+
     return 0;
 }
 
 void bt_i2c_exit(void){
-    platform_driver_unregister(&bt_i2c_driver);
+    i2c_del_driver(&bt_i2c_driver);
     printk(KERN_INFO "bt_i2c_exit.\n");
 }
 
