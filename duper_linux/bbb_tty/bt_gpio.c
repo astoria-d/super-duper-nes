cf196681f8b66e12191ca9c6aba80e31978de2fa
astoria-d <astoria-d@home-ubuntu>
2018-01-05 10:40:52 +0900

    gpio interrupt mode ok.

--- duper_linux/bbb_tty/bt_gpio.c
+++ duper_linux/bbb_tty/bt_gpio.c
@@ -2,6 +2,8 @@
 #include <linux/kernel.h>
 #include <linux/of.h>
 #include <linux/gpio.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
 
 #include "bbb_tty.h"
 
@@ -21,14 +23,49 @@ int bt_gpio_proc_show(struct seq_file *seq) {
     return 0;
 }
 
+static irq_handler_t bt_gpio_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs){
+    printk(KERN_INFO "gpio interrupt. irq:%d\n", irq);
+    return (irq_handler_t) IRQ_HANDLED;
+}
+
+static int gpio_bbb_f_emp_irq;
+static int gpio_nes_f_ful_irq;
 int bt_gpio_init(void){
+    int ret;
 
-    printk(KERN_INFO "gpio init.\n");
+    /*initialize gpio pin*/
+    gpio_request(GPIO_BBB_FIFO_EMPTY, "sysfs");
+    gpio_direction_input(GPIO_BBB_FIFO_EMPTY);
+    gpio_bbb_f_emp_irq = gpio_to_irq(GPIO_BBB_FIFO_EMPTY);
 
-    return 0;
+    gpio_request(GPIO_NES_FIFO_FULL, "sysfs");
+    gpio_direction_input(GPIO_NES_FIFO_FULL);
+    gpio_nes_f_ful_irq = gpio_to_irq(GPIO_NES_FIFO_FULL);
+
+    /*register interrupt hander.
+     triggered when fifo is not empty.
+     bbb_tty_gpio_handler appears @ /proc/interrupts */
+    ret = request_irq(gpio_bbb_f_emp_irq, (irq_handler_t) bt_gpio_irq_handler,
+            IRQF_TRIGGER_FALLING, "bbb_tty_bbb_f_emp", NULL);
+    if (ret) {
+        printk(KERN_INFO "error! irq:%d, ret:%d\n", gpio_bbb_f_emp_irq, ret);
+    }
+    ret = request_irq(gpio_nes_f_ful_irq, (irq_handler_t) bt_gpio_irq_handler,
+            IRQF_TRIGGER_FALLING, "bbb_tty_nes_f_ful", NULL);
+    if (ret) {
+        printk(KERN_INFO "error! irq:%d, ret:%d\n", gpio_nes_f_ful_irq, ret);
+    }
+
+    printk(KERN_INFO "gpio init done.\n");
+
+    return ret;
 }
 
 void bt_gpio_exit(void){
+    free_irq(gpio_bbb_f_emp_irq, NULL);
+    free_irq(gpio_nes_f_ful_irq, NULL);
+    gpio_free(GPIO_BBB_FIFO_EMPTY);
+    gpio_free(GPIO_NES_FIFO_FULL);
     printk(KERN_INFO "gpio exit.\n");
 }
 
