19a92cd6e1f90a166fd937f8e71c9a5ad42fe1b9
astoria-d <astoria-d@home-ubuntu>
2018-01-05 17:48:00 +0900

    tty device write ok...

--- duper_linux/bbb_tty/bbb_tty.c
+++ duper_linux/bbb_tty/bbb_tty.c
@@ -7,6 +7,10 @@
 
 #include "bbb_tty.h"
 
+static const struct tty_port_operations bt_port_ops = {
+};
+static struct tty_port    bbb_tty_port;
+
 static int nes_tty_write(struct tty_struct * tty,
         const unsigned char *buf, int c) {
     int i;
@@ -20,30 +24,57 @@ static int nes_tty_write(struct tty_struct * tty,
     return i;
 }
 
-static int nes_tty_write_room(struct tty_struct *tty)
-{
+static int nes_tty_write_room(struct tty_struct *tty) {
         return 1;
+}
+
+static int nes_tty_install(struct tty_driver *drv, struct tty_struct *tty) {
+    int ret;
+
+    tty_port_init(&bbb_tty_port);
+    bbb_tty_port.ops = &bt_port_ops;
+
+    ret = tty_port_install(&bbb_tty_port, drv, tty);
+    if (ret) {
+        pr_err("tty_port_install failed.\n");
+        return ret;
+    }
+
+    return 0;
+}
+
+static void nes_tty_cleanup(struct tty_struct *tty) {
+    tty_port_destroy(&bbb_tty_port);
+}
+
+static int nes_tty_open(struct tty_struct *tty, struct file *file) {
+    return tty_port_open(&bbb_tty_port, tty, file);
+}
+
+static void nes_tty_close(struct tty_struct *tty, struct file *file) {
+    tty_port_close(&bbb_tty_port, tty, file);
+}
+
+static void nes_tty_hangup(struct tty_struct *tty) {
+    tty_port_hangup(&bbb_tty_port);
 }
 
 
 static const struct tty_operations nes_tty_ops = {
     .write      = nes_tty_write,
     .write_room = nes_tty_write_room,
-/*
-    .open       = nes_tty_open,
-    .close      = nes_tty_close,
     .install    = nes_tty_install,
     .cleanup    = nes_tty_cleanup,
+    .open       = nes_tty_open,
+    .close      = nes_tty_close,
     .hangup     = nes_tty_hangup,
-*/
 };
 
 
 static struct tty_driver *bbb_tty_driver;
 
 static void nes_console_write (struct console *co, const char *str,
-                       unsigned int count)
-{
+                       unsigned int count) {
     while (count--) {
         bt_i2c_putchr( *str++ );
     }
@@ -66,7 +97,6 @@ static struct console nes_console_driver = {
     .flags      = CON_PRINTBUFFER,
     .index      = -1,
 };
-
 
 int __init bbb_tty_init(void){
     int ret;
